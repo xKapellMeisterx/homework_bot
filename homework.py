13983c649ed9b300c1a16a925dc94ad9fbd0bcce
Александр Иванченко <xKapellMeisterx@yandex.ru>
2022-05-25 20:26:47 +0300

    "Спринт № 7, добавил запрос к API и парсинг статуса проверки задания."

--- homework.py
+++ homework.py
@@ -1,11 +1,18 @@
-...
+import os
+import logging
+from datetime import time
+
+import requests
+import telegram
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,38 +25,45 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logger = logging.getLogger(__name__)
+
 
 def send_message(bot, message):
-    ...
+    return bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    valid_json = dict()
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+        valid_json = response.json()
+    except requests.ConnectionError as e:
+        message = 'Ошибка соединения.'
+        logger.error(message)
+        send_message(message)
+    return valid_json
 
-    ...
 
-
-def check_response(response):
-
-    ...
+# def check_response(response):
+#
+#     ...
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    homework_name = homework.get('homework_name', 'Нет имени работы')
+    homework_status = homework['status']
+    verdict = HOMEWORK_STATUSES['homework_status']
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens():
-    ...
+# def check_tokens():
+#     ...
 
 
 def main():

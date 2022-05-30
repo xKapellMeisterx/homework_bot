0540740a6e2c1134c40f7e1e5a59f3b466d9884a
Александр Иванченко <xKapellMeisterx@yandex.ru>
2022-05-30 22:04:18 +0300

    "Спринт № 7, исправил замечания из 2-го ревью."

--- homework.py
+++ homework.py
@@ -39,14 +39,17 @@ HOMEWORK_VERDICT = {
 
 
 class WrongAPIResponseCodeError(Exception):
+    """Исключение не правильного ответа API."""
     pass
 
 
 class ConnectionError(Exception):
+    """Исключение ошибки запроса API."""
     pass
 
 
 class MissingKeysInDictionary(Exception):
+    """Исключение нехватки ключа."""
     pass
 
 
